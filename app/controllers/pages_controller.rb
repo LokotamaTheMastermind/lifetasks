ccd1e7b3c95e90525b3ebf10af9cd0b5b0ced32c
Plays Lokotama <lokotamathemastermind@gmail.com>
2020-08-02 22:50:41 +0100

    Editing project

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,16 @@ class PagesController < ApplicationController
   def contact
   end
 
-  def contact
-  end
-
   def about
+  	@title = "My Cool About Page"
+  	@description = "Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Curabitur aliquet quam id dui posuere blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus."
   end
 
   def home
+  	@projects = Project.all
+  end
+
+  def error
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/contact'
-  get 'pages/contact'
-  get 'pages/about'
-  get 'pages/home'
+  get 'contact', to: "pages#contact"
+  get 'about', to: "pages#about"
+  get 'error', to: "pages#error"
+  get 'website', to: redirect("https://code-4-the-future7.webnode.com/")
   resources :projects
+  root 'pages#home'
+  get "*path", to: redirect("/error")
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
